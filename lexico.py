ddf55155719b13a36bd8a0dc490e21563b06d97e
Cindy Bohorquez <cinaboho@espol.edu.ec>
2022-11-13 21:08:46 -0500

    Update lexico.py

---
+++ lexico.py
@@ -0,0 +1,111 @@
+import ply.lex as lex
+
+reserved = {
+    #Cindy
+    'as': 'AS',
+    'rsort': 'RSORT',
+    'count': 'COUNT',
+    'array': 'ARRAY',
+    'global': 'GLOBAL',
+    'static': 'STATIC',
+    'print': 'PRINT',
+    'const': 'CONST',
+    'function': 'FUNCTION',
+    'return': 'RETURN',
+    'class': 'CLASS',
+    'new': 'NEW',
+    #Fin Cindy
+}
+tokens = [
+    #Cindy
+    'MENOS',
+    'MULTIPLICA',
+    'DIVIDE',
+    'MODULO',
+    'EXPONENCIACION',
+    'MASIGUAL',
+    'MENOSIGUAL',
+    'ASTERISCOIGUAL',
+    'BARRAIGUAL',
+    'PORCENTAJEIGUAL',
+    'DOBLEASTERISCOIGUAL',
+    'OPERCOMPARACION',
+    'OPERLOGICO_OR',
+    'OPERLOGICO_AND',
+    'OPERLOGICO_XOR',
+    'OPERLOGICO_OREXCLUSIVO',
+    'OPERLOGICO_NOT',
+    'OPERASIG_ARRAY',
+    #Fin Cindy
+ ] + list(reserved.values())
+ 
+ #Cindy
+ t_MENOS = r'\-'
+ t_MULTIPLICA = r'\*'
+t_DIVIDE = r'/'
+t_MODULO = r'%'
+t_EXPONENCIACION = r'\*\*'
+t_MASIGUAL = r'\+\='
+t_MENOSIGUAL = r'\-\='
+t_ASTERISCOIGUAL = r'\*\='
+t_BARRAIGUAL = r'\/\='
+t_PORCENTAJEIGUAL = r'\%\='
+t_DOBLEASTERISCOIGUAL = r'\*\*\='
+t_OPERCOMPARACION = r'=='
+
+def t_OPERLOGICO_OR(t):
+    r'or'
+    return t
+    
+def t_OPERLOGICO_AND(t):
+    r'and'
+    return t
+    
+def t_OPERLOGICO_XOR(t):
+    r'xor'
+    return t
+    
+def t_OPERLOGICO_OREXCLUSIVO(t):
+    r'\|\|'
+    return t
+
+
+def t_OPERLOGICO_NOT(t):
+    r'!'
+    return t
+    
+def t_OPERASIG_ARRAY(t):
+    r'(\=){1}(\>){1}'
+
+#Fin Cindy             
+        
+#Cindy
+resultados = []
+
+
+def t_error(t):
+    lineae="Caracter no reconocido {t.value[0]} en línea {t.lineno}"
+    print(lineae)
+    resultados.append(lineae)
+    t.lexer.skip(1)
+
+
+validador = lex.lex()
+def getTokens(lex):
+    while True:
+        tok = lex.token()
+        if not tok:
+            break
+        print(tok)
+
+
+linea = " "
+codigo = open("source.txt")
+for linea in codigo:
+    validador.input(linea)
+    getTokens(validador)
+codigo.close()
+
+print("Analisis Terminado: ")
+
+#FinCindy
